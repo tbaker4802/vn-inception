5e5e53df289a64a93cde0e73cb854a42d2851c71
Matt Hippely <hippely@gmail.com>
2014-09-22 13:25:48 -0700

    Add css fiels and combine app resources into one array.

--- src/vn-inception.js
+++ src/vn-inception.js
@@ -1,19 +1,22 @@
-(function(angular) {
-    angular.element(document).ready(function () {
-        window.name = 'NG_DEFER_BOOTSTRAP!';
-        var appScripts = [];
-        var appModuleNames = [];
-        angular.injector(['ng']).invoke(function($http) {
-            $http.get('settings/themeInfo.json').then(function (response) {
-                angular.forEach(response.data.apps, function(app) {
-                    appScripts.push(app.scriptSrc);
-                    appModuleNames.push(app.appModuleName);
+(function (angular) {
+	angular.element(document).ready(function () {
+		window.name = 'NG_DEFER_BOOTSTRAP!';
+		var appResources = [],
+			appModuleNames = [];
 
-                    head.load(appScripts, function () {
-                        angular.resumeBootstrap(appModuleNames);
-                    });
-                });
-            });
-        });
-    });
+		angular.injector(['ng']).invoke(function ($http) {
+			$http.get('settings/themeInfo.json').then(function (response) {
+				angular.forEach(response.data.apps, function (app) {
+					appResources.push(app.scriptSrc);
+					appResources.push(app.styleSrc);
+					appModuleNames.push(app.appModuleName);
+				});
+
+				head.load(appResources, function () {
+					angular.resumeBootstrap(appModuleNames);
+				});
+
+			});
+		});
+	});
 })(angular);
