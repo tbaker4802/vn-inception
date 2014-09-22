fd300d6431325052fb055f7d255ef9610e589fcc
Prabin Varma <prabinv@gmail.com>
2014-09-22 11:27:24 -0500

    Removed jQuery dependency

--- src/theme-dream.js
+++ src/theme-dream.js
@@ -1,22 +1,19 @@
-(function ($, angular) {
+(function(angular) {
+    angular.element(document).ready(function () {
+        window.name = 'NG_DEFER_BOOTSTRAP!';
+        var appScripts = [];
+        var appModuleNames = [];
+        angular.injector(['ng']).invoke(function($http) {
+            $http.get('settings/themeInfo.json').then(function (response) {
+                angular.forEach(response.data.apps, function(app) {
+                    appScripts.push(app.scriptSrc);
+                    appModuleNames.push(app.appModuleName);
 
-	angular.element(document).ready(function () {
-		$.getJSON('settings/themeInfo.json', function (data) {
-			window.name = 'NG_DEFER_BOOTSTRAP!';
-			var appScripts = [];
-			var appModuleNames = [];
-
-			$.each(data.apps, function (index, app) {
-				appScripts.push(app.scriptSrc);
-				appModuleNames.push(app.appModuleName);
-			});
-
-			head.load(appScripts, function () {
-				angular.resumeBootstrap(appModuleNames);
-			});
-
-		});
-	});
-})($, angular);
-
-
+                    head.load(appScripts, function () {
+                        angular.resumeBootstrap(appModuleNames);
+                    });
+                });
+            });
+        });
+    });
+})(angular);
